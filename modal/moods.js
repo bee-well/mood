f468d29e33d18be4431c302bd6690dafec556997
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-21 15:06:11 +0200

    modified to make sure all essential packages were installed

--- modal/moods.js
+++ modal/moods.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const { timeStamp } = require("node:console");
 const Schema = mongoose.Schema;
 
 const moodSchema = new Schema({
     id : int,
     user: int,
     mood: int,
-     tags: string,
-     timeStamp: true
+    tags: string,
+    timeStamp: true
  });
 
 const Mood= mongoose.model('Mood', moodSchema);
